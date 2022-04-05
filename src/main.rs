5d3a90f6033b9d517009d9b32ae17f02847d12e0
Shimpei Otsubo <potsbo@gmail.com>
2022-04-06 00:44:42 +0900

    Add initial impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use chrono::{Datelike, NaiveDate, Weekday};
+use rand::Rng;
+use std::io;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let ce = NaiveDate::from_num_days_from_ce(0);
+
+    // TODO: this should be configurable via command line options
+    let begin = (NaiveDate::from_ymd(1980, 1, 1) - ce).num_days();
+    let end = (NaiveDate::from_ymd(2039, 12, 31) - ce).num_days();
+
+    let d = rng.gen_range(begin..=end);
+    let date = NaiveDate::from_num_days_from_ce(d.try_into().unwrap());
+
+    println!("{}", date);
+    let ans = date.weekday();
+    let now = Instant::now();
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        match guess.trim().parse::<Weekday>() {
+            Ok(wd) => {
+                if ans == wd {
+                    println!("Correct!");
+                    println!("{}ms", now.elapsed().as_millis());
+                    break;
+                }
+                println!("Retry");
+            }
+            Err(e) => println!("failed to parse as a weekday. Retry, {:?}", e),
+        };
+    }
 }
